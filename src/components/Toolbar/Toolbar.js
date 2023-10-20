41ca8fe3634168713b853125323aad89b7d035eb
nandodani <nando.apostolo.fd@gmail.com>
2023-10-20 16:04:11 +0100

    Build

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,26 +1,20 @@
-import React, { useState } from "react";
-import {
-  IonHeader,
-  IonToolbar,
-  IonButton,
-  IonButtons,
-  IonTitle,
-} from "@ionic/react";
+import React from "react";
+import { IonToolbar, IonButton, IonButtons, IonTitle } from "@ionic/react";
 import SettingsMenu from "../SettingsMenu/SettingsMenu";
 import { IconMenu } from "@tabler/icons-react";
 
 const Toolbar = () => {
   return (
     <>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonButton onClick={() => console.log("Settings")}>
-              <IconMenu style={{ color: "white" }}/>
-            </IonButton>
-          </IonButtons>
-          <IonTitle style={{ fontFamily: "Satoshi-Variable" }}>Cidade</IonTitle>
-        </IonToolbar>
-        <SettingsMenu />
+      <IonToolbar>
+        <IonButtons slot="start">
+          <IonButton onClick={() => console.log("Settings")}>
+            <IconMenu style={{ color: "white" }} />
+          </IonButton>
+        </IonButtons>
+        <IonTitle style={{ fontFamily: "Satoshi-Variable" }}>Cidade</IonTitle>
+      </IonToolbar>
+      <SettingsMenu />
     </>
   );
 };
